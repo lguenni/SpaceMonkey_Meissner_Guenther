7bde63d084dfbcd62cca9658f4c8f4bc40763098
meissner <meisfa01@thu.de>
2023-01-10 12:03:59 +0100

    Kommentare entfernt

--- Test/SpielweltTest.java
+++ Test/SpielweltTest.java
@@ -9,16 +9,6 @@ public class SpielweltTest {
     Schuss testSchuss = new Schuss(testwelt,300,300);
 
 
-
-    //Habs doch irgendwie geschafft, schau mal ob es verstehst
-    //Zeiten wurden in Sekunden umgerechnet weil weniger Abweichung bei Durchläufen
-    //Haben aber immernoch Abweichungen von Circa ner Sekunde aber die ist random
-
-    //Kontrolliere ob die erste Zeit kleiner oder gleich 60s ist
-    //aber erste Zeit darf noch nicht kleiner oder gleich 57s sein (das wären mehr als 3s Abweichung so viel haben wir nicht)
-
-    //und ob Zeit nach 2s Verzögerung kleiner oder gleich 58s ist
-
     @Test
     public void testGetVerbleibendeZeit() throws InterruptedException {
         int testVerbleibendeSekunden = testwelt.getVerbleibendeZeit() / 1000;
@@ -27,17 +17,10 @@ public class SpielweltTest {
         Thread.sleep(2000);
         int testVerbleibendeSekundenNachWarten = testwelt.getVerbleibendeZeit() / 1000;
 
-        //System.out.println(testVerbleibendeSekunden);
-        //System.out.println(testVerbleibendeSekundenNachWarten);
-        //Mit den Prints versteht man es eigentlich ganz gut
-        //einfach mal auskommentieren und paar mal durchlaufen lassen dann sieht auch die Abweichung :)
-
         Assert.assertTrue(testVerbleibendeSekunden <= testMaxSekunden);
         Assert.assertFalse(testVerbleibendeSekunden <= 57);
 
         Assert.assertTrue(testVerbleibendeSekundenNachWarten <= testMaxSekunden - 2);
-
-
     }
 
     @Test
@@ -56,9 +39,7 @@ public class SpielweltTest {
         Assert.assertEquals(2,testwelt.getAnzahlSchuesse());
     }
 
-    //Können nur abfragen ob ein Schuss erzeugt wurde
-    //Genaue Position abfragen wär au wichtig aber die Liste Schüsse ist ja privat
-    //Hab noch einen zweiten Test gemacht um die Höhenkontrolle abzufragen
+
     @Test
     public void testErzeugeSchuss() {
         testwelt.raumschiff = new Apollo404(testwelt, 300, 600);
@@ -125,9 +106,7 @@ public class SpielweltTest {
         Assert.assertEquals(2,testwelt.getAnzahlZuLoeschen());
     }
 
-    //Hab einen Affe und einen Schuss ohne Kollison und einen Schuss mit Kollision erzeugt
-    //Der Schuss mit Kollsion wird gelöscht. Ich ruf aufräumen auf damit er aus der Liste gelöscht wird
-    //Nachdem der kollidierte Schuss gelöscht wurde darf in der Liste nur noch einer drin sein
+
     @Test
     public void testPruefeKollisionenMonkeySchuss(){
         testwelt.addMonkey(new MarsMonkey(testwelt, 200, 200));
@@ -143,9 +122,7 @@ public class SpielweltTest {
         Assert.assertEquals(1, testwelt.getAnzahlSchuesse());
     }
 
-    //Gleiches Prinzip nochmal
-    //Ein schuss trifft meteorit weil er den meteorit berührt
-    //Anderer Schuss ist 1 Pixel weiter oben
+
     @Test
     public void testPruefeKollisionenMeteoritSchuss(){
         testwelt.meteorit = new Meteorit(testwelt, 200, 200);
